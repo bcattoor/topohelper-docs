32b0287f41acb4a66497e195cfa20019b688d6d4
BC <orhiannono@gmail.com>
2020-11-16 10:29:09 +0100

    Update PS_local_build.ps1
    Added github pushing to the powershell script.
    Some code refractoring.

--- PS_local_build.ps1
+++ PS_local_build.ps1
@@ -1,43 +1,58 @@
 # functions
 
-function DeleteFolders ($Folders) {
+function Remove-Folders {
+    [CmdletBinding()]
+    param([parameter(Mandatory = $true)] $Folders) 
     if ( $Folders -eq "" -or $Folders.Count -eq 0 ) {
-        Write-Output "No folders to delete."
+        Write-Verbose "No folders to delete."
     }
     else {
         foreach ($Folder in $Folders) {
-            
-            DeleteFolder $Folder
-            Write-Output "Deleted $Folder"
+            Remove-Folder $Folder
         } 
     }
 }
 
-function DeleteFolder ($dir ) {
-    if ( Test-Path $dir ) {
+function Remove-Folder {
+    [CmdletBinding()]
+    param ([parameter(Mandatory = $true)][String] $FolderPath ) 
+    if ( Test-Path $FolderPath ) {
         #http://stackoverflow.com/questions/7909167/how-to-quietly-remove-a-directory-with-content-in-powershell/9012108#9012108
-        Get-ChildItem -Path  $dir -Force -Recurse | Remove-Item -Force -Recurse
-        Remove-Item $dir -Force
+        Get-ChildItem -Path  $FolderPath -Force -Recurse | Remove-Item -Force -Recurse
+        Remove-Item $FolderPath -Force
+        Write-Verbose "Deleted folder: $FolderPath"
     }
 }
 
-function DeleteFiles ($FilesToDelete ) {
+function Remove-Files {
+    [CmdletBinding()]
+    param ([parameter(Mandatory = $true)] $FilesToDelete ) 
     if ( $FilesToDelete -eq "" -or $FilesToDelete.Count -eq 0 ) {
-        write-host "No files to delete."
+        Write-Verbose "No files to delete."
     }
     else {
         foreach ($File in $FilesToDelete) {
-            Remove-Item $File -Force -ErrorAction SilentlyContinue
-            write-host "Deleted $File"
+            if ( Test-Path $File ) {
+                Remove-Item $File -Force -ErrorAction SilentlyContinue
+                Write-Verbose "Deleted file:  $File"
+            }
         } 
     }
 }
 
+function Push-GitRepository {
+    param ([parameter(Mandatory = $true)] $FolderContext) 
+    # Publish changes to github
+    $WorkingFolder = Get-Location
+    Set-Location $FolderContext
+    git add -A
+    git commit -m "Update from PS."
+    git push
+    Set-Location $WorkingFolder
+}
+
 function Stop-Processes {
-    param(
-        [parameter(Mandatory = $true)] $processName,
-        $timeout = 5
-    )
+    param([parameter(Mandatory = $true)] $processName, $timeout = 5)
     $processList = Get-Process $processName -ErrorAction SilentlyContinue
     if ($processList) {
         # Try gracefully first
@@ -45,52 +60,52 @@ function Stop-Processes {
 
         # Wait until all processes have terminated or until timeout
         for ($i = 0 ; $i -le $timeout; $i ++) {
-            $AllHaveExited = $True
-            $processList | % {
-                $process = $_
-                If (!$process.HasExited) {
-                    $AllHaveExited = $False
+            $processList | ForEach-Object {
+                If (!$_.HasExited) {
+                    Start-Sleep 1
                 }                    
             }
-            If ($AllHaveExited) {
-                Return
-            }
-            sleep 1
         }
         # Else: kill
         $processList | Stop-Process -Force        
     }
 }
 
-#########################################################
-#########################################################
-#########################################################
+################################################################################
+###############CODE####STARTS####HERE###########################################
+################################################################################
+$DestinationPath = "F:\Source\Repos\documentation\topohelper-github-pages"
+$SourcePath = ".\_build\html"
+$PrefferedBrowser = "msedge"
 
-Stop-Processes "msedge"
+
+
+Stop-Processes $PrefferedBrowser
 ./make clean
 ./make html
 
-#########################################################
-#########################################################
-# Copy generated files to the topohelper repository responsible for publish to github
-$DestinationPath = "F:\Source\Repos\documentation\topohelper-github-pages"
-$SourcePath = ".\_build\html"
-
-#  get all child folders who are not named ".git", delete these childfolders
+# Copy generated files to the topohelper repository responsible for publishing
+# GO GET all child folders who are not named ".git", delete these childfolders
 $FoldersToDelete = Get-ChildItem $DestinationPath -Directory -ErrorAction SilentlyContinue -Exclude ".git"
-DeleteFolders $FoldersToDelete
+Remove-Folders $FoldersToDelete
 
 # We also delete files in root folder
 $FilesToDelete = Get-ChildItem -Path $DestinationPath -File
-DeleteFiles $FilesToDelete
+Remove-Files $FilesToDelete
 
 # Copy new files to the destination, we don't use force here, becouse the folder
 # should be empty
 Copy-Item $SourcePath\* $DestinationPath -recurse
 Write-Host "New files were written to the github pages publish-folder here: $DestinationPath"
 
+Push-GitRepository $DestinationPath
+
 #########################################################
 #########################################################
+$urls = @(
+    "F:\Source\Repos\documentation\topohelper-docs\_build\html\welcome.html", 
+    "https://bcattoor.github.io/topohelper/")
 
-
-explorer "F:\Source\Repos\documentation\topohelper-docs\_build\html\welcome.html"
+foreach ($url in $urls) {
+    explorer $url
+}
