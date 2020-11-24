5bb1c6bd61804c2cef22458c729fda06a55b47c0
BC <orhiannono@gmail.com>
2020-11-24 18:26:34 +0100

    Catching push errors from git.exe call

--- build.ps1
+++ build.ps1
@@ -47,10 +47,15 @@ function Push-GitRepository {
     # Publish changes to github
     $WorkingFolder = Get-Location
     Set-Location $FolderContext
-    git add -A
-    git commit -m "Update from PS."
-    git push
-    Set-Location $WorkingFolder
+    try {  
+        Invoke-Utility git add -A
+        Invoke-Utility git commit -m "Update from PS."
+        Invoke-Utility git push
+    }
+    catch { Throw }
+    finally {
+        Set-Location $WorkingFolder
+    }
 }
 
 function Stop-Processes {
@@ -80,6 +85,38 @@ function Stop-Processes {
     else {
         Write-Verbose "Did not find Process to stop: $processName"
     }
+}
+
+function Open-WithExplorer {
+    [CmdletBinding()]
+    param ([parameter(Mandatory = $true)][string] $Url ) 
+    Write-Verbose "|___/--> Opening $Url" 
+    explorer $Url 
+}
+
+function Invoke-Utility {
+    <#
+.SYNOPSIS
+Invokes an external utility, ensuring successful execution.
+https://stackoverflow.com/a/48877892
+
+.DESCRIPTION
+Invokes an external utility (program) and, if the utility indicates failure by 
+way of a nonzero exit code, throws a script-terminating error.
+
+* Pass the command the way you would execute the command directly.
+* Do NOT use & as the first argument if the executable name is not a literal.
+
+.EXAMPLE
+Invoke-Utility git push
+
+Executes `git push` and throws a script-terminating error if the exit code
+is nonzero.
+#>
+    $exe, $argsForExe = $Args
+    $ErrorActionPreference = 'Continue' # to prevent 2> redirections from triggering a terminating error.
+    try { & $exe $argsForExe } catch { Throw } # catch is triggered ONLY if $exe can't be found, never for errors reported by $exe itself
+    if ($LASTEXITCODE) { Throw "$exe indicated failure (exit code $LASTEXITCODE; full command: $Args)." }
 }
 
 ################################################################################
